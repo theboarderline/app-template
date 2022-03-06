5833c91dcc29ec481274a8dd7cc00b9d6c126a67
Walker O'Brien <walkernobrien@gmail.com>
2022-03-06 01:35:45 -0600

    wi script

--- bin/workload_identity.sh
+++ bin/workload_identity.sh
@@ -4,19 +4,23 @@ source ./bin/check_env.sh
 
 
 LIFECYCLES=(
-  "o"
-  "d"
-  "t"
-  "s"
-  "p"
+  "ops"
+  "dev"
+  "test"
+  "stage"
+  "prod"
 )
 
 for CYCLE in ${LIFECYCLES[@]}; do
-  GKE_PROJECT="$CYCLE-$PROJ_IDENTIFIER-gke-project"
+  LIFECYCLE_LETTER="${CYCLE:0:1}"
+  GKE_PROJECT="$LIFECYCLE_LETTER-$PROJ_IDENTIFIER-gke-project"
 
   gcloud iam service-accounts add-iam-policy-binding \
     --project $APP_PROJECT \
     --role roles/iam.workloadIdentityUser \
-    --member "serviceAccount:$GKE_PROJECT.svc.id.goog[$NAMESPACE/$APP_CODE-sa]" \
+    --member "serviceAccount:$GKE_PROJECT.svc.id.goog[$CYCLE-$NAMESPACE/$CYCLE-$NAMESPACE-sa]" \
     $NAMESPACE-workload@$APP_PROJECT.iam.gserviceaccount.com
 done
+
+
+
